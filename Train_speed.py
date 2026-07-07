439b6004bf170e27a079da60ad36243524d4aeb6
Ananth <96000989+ananthreddy03@users.noreply.github.com>
2026-07-07 23:13:29 +0530

    Update Train_speed.py

--- Train_speed.py
+++ Train_speed.py
@@ -1,14 +1,13 @@
-def time(dist,speed):
- 
- speed=int(input("Enter the speed of the train: "))
- if(speed<1 or speed>200):
-  print("Error")
-  return None
-   
- else:
-  time_taken=(dist/speed)*(18/5)
-  return time_taken
+def time(dist, speed):
+    if speed < 1 or speed > 200:
+        print("Error")
+        return None
 
-result=time(400, speed)
+    time_taken = (dist / speed) * (18 / 5)
+    return time_taken
+
+speed = int(input("Enter the speed of the train: "))
+result = time(400, speed)
+
 if result is not None:
-  print(result)
+    print(result)
